c2ba7a2909f0b4457355057de159361c30a84fea
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2021-06-11 20:42:08 -0500

    initial commit

---
+++ Card.java
@@ -0,0 +1,12 @@
+public class Card {
+    String suit;
+    String value;
+    boolean is_face_up = false;
+    int rank;
+
+    Card(String suit, String value){
+        this.suit = suit;
+        this.value = value;
+        this.rank = Rules.get_rank(value);
+    }
+}
---
+++ src/com/joshuarodgers/Game.java
@@ -0,0 +1,10 @@
+package com.joshuarodgers;
+
+import java.util.Arrays;
+import java.util.function.*;
+
+class Game{
+    public static void main(String[] args) {
+        
+    }
+}
