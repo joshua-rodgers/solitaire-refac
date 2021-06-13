524cb17a293cf627c2539ef1b67fb686d4c1d3f9
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2021-06-12 21:19:38 -0500

    Cards shuffled. test version
    method shuffle deck creates an array of randomly ordered cards and prints them to the console for verification. committing this before packaging the method into it final version that does its job without printing anything.

---
+++ compile_run.bat
@@ -0,0 +1,5 @@
+del bin\*.class
+javac src\com\joshuarodgers\*.java -d bin
+cd bin
+java com.joshuarodgers.Game
+cd ..
--- src/com/joshuarodgers/Game.java
+++ src/com/joshuarodgers/Game.java
@@ -1,9 +1,13 @@
 package com.joshuarodgers;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Stack;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PrimitiveIterator;
+import java.util.Random;
 
 class Game{
     Stack<Card> deck;
@@ -12,15 +16,25 @@ class Game{
     Game(){
         deck = new Stack<>();
         init_deck();
+        shuffle_deck();
     }
 
     private void init_deck(){
         List<String> suits = Arrays.asList("\3", "\4", "\5", "\6");
         suits.stream().forEach(s->Rules.ranking.keySet().stream().forEach(v->deck.push(new Card(s, v))));
-        deck.stream().forEach(s->System.out.println(s.suit + " " + s.value));
-        System.out.println(deck.size());
     }
-    
+
+    private void shuffle_deck(){
+        Random r = new Random();
+        List<Integer> nums = new ArrayList<>();
+        PrimitiveIterator.OfInt i = IntStream.range(0,52).iterator();
+        IntConsumer ic = n->nums.add(Integer.valueOf(n));
+        i.forEachRemaining(ic);
+        Card[] new_deck = new Card[52];
+        deck.stream().forEach(c->new_deck[nums.remove(r.nextInt(nums.size()))] = c);
+        Arrays.stream(new_deck).forEach(c -> System.out.print(c.suit + " " + c.value + ", "));
+    }
+
     public static void main(String[] args) {
         new Game();
     }
